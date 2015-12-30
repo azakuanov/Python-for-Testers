5ec374265c84bfa298f5ee9e3918fcf22cef434d
azakuanov <azat.zakuanov@yahoo.com>
2015-12-29 23:47:38 -0800

    vopros pro IndexError: list index out of range

--- fixture/contact.py
+++ fixture/contact.py
@@ -107,5 +107,38 @@ class ContactHelper:
                 firstname = cells[2].text
                 lastname  = cells[1].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id))
+                all_phones = cells[5].text.splitlines()
+                print(all_phones)
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                                                  homephone = all_phones[0], mobilephone = all_phones[1],
+                                                  workphone = all_phones[2], secondaryphone = all_phones[2]))
         return list(self.contact_cache)
+
+    def get_contact_to_edit_by_index (self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name ('entry') [index]
+        cell = row.find_elements_by_tag_name ("td") [7]
+        cell.find_element_by_tag_name("a").click()
+
+    def get_contact_view_by_index (self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name ('entry') [index]
+        cell = row.find_elements_by_tag_name ("td") [6]
+        cell.find_element_by_tag_name("a").click()
+
+
+    def get_contact_info_from_edit_page (self, index):
+        wd = self.app.wd
+        self.get_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname = firstname, lastname = lastname, id = id,
+                       homephone = homephone, workphone = workphone,
+                       mobilephone = mobilephone, secondaryphone = secondaryphone)
--- model/contact.py
+++ model/contact.py
@@ -2,7 +2,7 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self ,firstname = None, middlename = None, lastname = None, nickname = None, title = None, company = None, address = None, id = None):
+    def __init__(self ,firstname = None, middlename = None, lastname = None, nickname = None, title = None, company = None, address = None, id = None, homephone = None, workphone = None, mobilephone = None, secondaryphone = 0):
 
         self.firstname = firstname
         self.middlename = middlename
@@ -12,6 +12,10 @@ class Contact:
         self.company = company
         self.address = address
         self.id = id
+        self.homephone = homephone
+        self.workphone = workphone
+        self.mobilephone = mobilephone
+        self.secondaryphone = secondaryphone
 
     def __repr__(self):
         return "%s, %s, %s" % (self.id, self.firstname, self.lastname)
