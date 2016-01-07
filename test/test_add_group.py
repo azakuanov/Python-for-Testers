73209b59b76fcf4baafcec3072ef0bdc0abca556
azakuanov <azat.zakuanov@yahoo.com>
2016-01-06 23:13:07 -0800

    Zadanie#16

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,8 +1,21 @@
 from model.group import Group
+import pytest
+import random
+import string
+
+def random_string (prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range( random.randrange(maxlen))])
 
 
+test_data = [Group(name = "", header = "", footer = "")] + [
 
-def test_add_group(app):
+        Group(name = random_string("name", 10), header = random_string("header", 20), footer = random_string("footer", 20))
+        for i in range(5)
+    ]
+
+@pytest.mark.parametrize("group", test_data, ids = [repr(x) for x in test_data])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
     group = Group(name = "RandomName", header = "RandomHeader", footer = "RandomFooter")
     app.group.create_new_group(group)
